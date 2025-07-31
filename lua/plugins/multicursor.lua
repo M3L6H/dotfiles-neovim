accf352321818a410e84bd9f94294e41a136c221
m3l6h <8094643+M3L6H@users.noreply.github.com>
2025-07-31 11:47:30 -0500

    Add multicursor
    Fixes #3

---
+++ lua/plugins/multicursor.lua
@@ -0,0 +1,86 @@
+local lazyAdd = require("nixCatsUtils").lazyAdd
+
+local M = {
+  "jake-stewart/multicursor.nvim",
+  enabled = lazyAdd(vim.g.plugins.multicursor, nixCats("multicursor")),
+  keys = {
+    {
+      "<up>",
+      function() require("multicursor-nvim").lineAddCursor(-1) end,
+      mode = { "n", "x" },
+      desc = "Add cursor above",
+    },
+    {
+      "<down>",
+      function() require("multicursor-nvim").lineAddCursor(1) end,
+      mode = { "n", "x" },
+      desc = "Add cursor below",
+    },
+    {
+      "<leader>v",
+      function() require("multicursor-nvim").matchAddCursor(1) end,
+      mode = { "n", "x" },
+      desc = "Add cursor at next match",
+    },
+    {
+      "<leader>V",
+      function() require("multicursor-nvim").matchAddCursor(-1) end,
+      mode = { "n", "x" },
+      desc = "Add cursor at previous match",
+    },
+    {
+      "<C-leftmouse>",
+      require("multicursor-nvim").handleMouse,
+      mode = { "n", "x" },
+      desc = "Add cursor with click",
+    },
+    {
+      "<C-leftdrag>",
+      require("multicursor-nvim").handleMouseDrag,
+      mode = { "n", "x" },
+      desc = "Add cursors with drag",
+    },
+    {
+      "<C-leftrelease>",
+      require("multicursor-nvim").handleMouseRelease,
+      mode = { "n", "x" },
+      desc = "Stop adding cursors with drag",
+    },
+    {
+      "<C-q>",
+      require("multicursor-nvim").toggleCursor,
+      mode = { "n", "x" },
+      desc = "Toggle cursor disabled/enabled",
+    },
+  },
+  config = function()
+    local mc = require("multicursor-nvim")
+    mc.setup()
+
+    mc.addKeymapLayer(function(layerSet)
+      layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+      layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+
+      layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+      layerSet("n", "<esc>", function()
+        if not mc.cursorsEnabled() then
+          mc.enableCursors()
+        else
+          mc.clearCursors()
+        end
+      end)
+    end)
+
+    local hl = vim.api.nvim_set_hl
+    hl(0, "MultiCursorCursor", { reverse = true })
+    hl(0, "MultiCursorVisual", { link = "Visual" })
+    hl(0, "MultiCursorSign", { link = "SignColumn" })
+    hl(0, "MultiCursorMatchPreview", { link = "Search" })
+    hl(0, "MultiCursorDisabledCursor", { reverse = true })
+    hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+    hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+  end,
+}
+
+return M
